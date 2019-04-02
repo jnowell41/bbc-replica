0c80ef313e9ce7dbd8ef3489f59a62dac3427b4b
James Nowell <james.nowell@Nology.local>
2019-04-02 11:44:29 +0100

    getting there

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,18 @@
-var message = "hello world, does this work";
-console.log(message);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var newsData_1 = require("./newsData");
+var reveal = function () {
+    var menu = document.getElementById("menu");
+    menu.style.display == "none" ? menu.style.display = "block" : menu.style.display = "none";
+};
+;
+var newsData = newsData_1.default;
+var addToPage = function () {
+    var html = "<article>\n    <img src=\"http://www.placekitten.com/600/400\" alt=\"a cat\" />\n    <header>Intoxicated cat steals JCB, ploughs into local town hall</header>\n    <div class=\"description\">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor\n        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\n        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit\n        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa\n        qui officia deserunt mollit anim id est laborum.</div>\n</article>";
+    document.getElementById("container").innerHTML = html;
+};
+addToPage();
+// newsData.articles.forEach(element => {
+//     addToPage(element;)
+// });
 //# sourceMappingURL=main.js.map
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,62 @@
-let message:string = "hello world, does this work";
+import data from './newsData';
 
-console.log(message);
+const reveal = (): void => {
+    const menu = document.getElementById("menu");
+    menu.style.display == "none" ? menu.style.display = "block" : menu.style.display = "none";
+};
+
+// const refinedReveal = () => {
+//     const menu = document.getElementById("menu").classList.toggle("not_hidden");
+// };
+
+// (<any>window).refinedReveal = (e: MouseEvent) => {
+//     e.preventDefault();
+//     document.getElementById("menu").classList.toggle("not_hidden");
+// }
+// const getTime = ():void => {
+//     var date:string = new Date().toLocaleTimeString();
+//     document.getElementById("time-stamp").innerHTML=date;
+// };
+// getTime();
+
+interface INewsData {
+    status: string;
+    totalResults: number;
+    articles: IArticleData[];
+};
+
+interface IArticleData {
+    source: {
+        id: string;
+        name: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
+
+const newsData: INewsData = data;
+
+
+const addToPage = () => {
+    let html = `<article>
+    <img src="http://www.placekitten.com/600/400" alt="a cat" />
+    <header>Intoxicated cat steals JCB, ploughs into local town hall</header>
+    <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+        qui officia deserunt mollit anim id est laborum.</div>
+</article>`
+    document.getElementById("container").innerHTML = html;   
+}
+
+addToPage();
+
+// newsData.articles.forEach(element => {
+//     addToPage(element;)
+// });
